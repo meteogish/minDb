eaea2227acae3811068b72bc65da165d649713c9
meteogish <meteogish@gmail.com>
2018-03-03 20:14:43 +0100

    SqlParser something

--- ConsoleEntry/src/main/java/ConsoleEntry/App.java
+++ ConsoleEntry/src/main/java/ConsoleEntry/App.java
@@ -1,8 +1,13 @@
 package ConsoleEntry;
 
+import minDb.SqlQueryParser.*;
+import minDb.Core.Components.IQueryParser;
+
 public class App {
 
     public static void main(String[] args) {
+        IQueryParser parser = new SqlQueryParser();
+        System.out.println(parser.parse("statement") == null);
     }
 
 }
--- minDb.SqlQueryParser/src/main/java/minDb/SqlQueryParser/SqlQueryParser.java
+++ minDb.SqlQueryParser/src/main/java/minDb/SqlQueryParser/SqlQueryParser.java
@@ -1,9 +1,17 @@
-package minDb.SqlQuery;
+package minDb.SqlQueryParser;
+
+import minDb.Core.Components.IQueryParser;
+import minDb.Core.QueryModels.Column;
+import minDb.Core.QueryModels.Query;
+import minDb.Core.QueryModels.Table;
 
 /**
- * QueryParser
+ * SqlQueryParser
  */
-public class SqlQueryParser {
-    
-    
+public class SqlQueryParser implements IQueryParser{
+
+	public Query parse(String statement) {
+		Column c = null;
+        return null; 
+	}    
 }
